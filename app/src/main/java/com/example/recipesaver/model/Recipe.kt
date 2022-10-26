a05846f2202e5811b38546c2fac67b6303e732c5
Leonardo Conroy <leeleonard936@gmail.com>
2022-10-26 12:28:03 -0400

    recipe class

---
+++ app/src/main/java/com/example/recipesaver/model/Recipe.kt
@@ -0,0 +1,11 @@
+package com.example.recipesaver.model
+
+class Recipe (
+    val name: String,
+    val ingredient: Array<String>,
+    val time: Int,
+    val cook: Boolean,
+    val timeToCook: Int
+    ){
+
+}
